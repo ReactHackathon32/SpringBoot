6fa9d05f028f81567a4280401a8083833af9da65
ZawadulFarhan <zawadfarhan@gmail.com>
2023-01-17 01:18:20 +0800

    Initial Commit of Spring Boot App

---
+++ smart-parking-rest-app/src/main/java/com/accenture/hackathon/SmartParkingRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingRestAppApplication.class, args);
+	}
+
+}
---
+++ smart-parking-rest-app/src/test/java/com/accenture/hackathon/SmartParkingRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
